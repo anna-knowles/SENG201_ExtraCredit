ee99fb69bf76a05c20121e1a00ca1fbb078ea0df
Anna Knowles <annaknowles1253@gmail.com>
2024-11-17 13:30:25 -0500

    Added documentation

--- math_ops.py
+++ math_ops.py
@@ -3,7 +3,7 @@
 
 def divide_numbers(a, b):
     """Divides two numbers and returns the result."""
-    if b == 0:
+    if b == 0:      #Rather than throwing an exception, if program is divided by 0, it will return a nice message
         return "Cannot divide by 0"
     return a / b  # Potential division by zero error
 
@@ -14,8 +14,8 @@ def multiply_numbers(a, b):
 if __name__ == "__main__":
     x = 10
     y = 0
-    result = divide_numbers(x, y)
+    result = divide_numbers(x, y)                       #calls and tests divide_numbers function
     print(f"The result of division is: {result}")
 
-    productResult = multiply_numbers(x, y)
+    productResult = multiply_numbers(x, y)              #calls and tests multiply_numbers function
     print(f"The result of multiplication is: {productResult}")
